85fa74f9e3c7559fe79da4a59aa2d98600cf7cad
Piszczek <ognisty001@gmail.com>
2024-05-10 18:06:26 +0200

    randomized file name

--- app/new-event/page.tsx
+++ app/new-event/page.tsx
@@ -3,56 +3,82 @@ import { redirect } from "next/navigation";
 import BackButton from "@/components/BackButton";
 import { SubmitButton } from "@/components/SubmitButton";
 
-export default function Login({
+export default async function Login({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) return redirect("/login");
+
   const addEvent = async (formData: FormData) => {
     "use server";
+    const errors: string[] = [];
 
-    const title = formData.get("title") as string;
-    const image = formData.get("image") as File;
-    const description = formData.get("description") as string;
-    const location = formData.get("location") as string;
-    const date = formData.get("date") as string;
+    try {
+      const title = formData.get("title") as string;
+      const image = formData.get("image") as File;
+      const description = formData.get("description") as string;
+      const location = formData.get("location") as string;
+      const date = formData.get("date") as string;
 
-    const supabase = createClient();
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    if (!user) return redirect("/new-event?message=You have to be logged in");
+      const supabase = createClient();
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      if (!user) throw new Error("You have to be logged in");
 
-    const { error } = await supabase.storage
-      .from("event-photo")
-      .upload("public/" + image.name, image);
+      const randomId = Math.random().toString(36).substring(2);
+      let extension = "";
+      const filenameParts = image.name.split(".");
+      if (filenameParts.length > 1) {
+        extension = filenameParts[filenameParts.length - 1].toLowerCase();
+      }
+      if (!["jpg", "jpeg"].includes(extension)) {
+        throw new Error("Wrong file format. Please upload a JPG file.");
+      }
 
-    if (error) {
-      console.log(error);
-      return redirect("/new-event?message=Could not create an event");
+      const { error: uploadError } = await supabase.storage
+        .from("event-photo")
+        .upload(`public/${randomId}.${extension}`, image);
+
+      if (uploadError) {
+        throw new Error("Failed to upload image. Please try again.");
+      }
+
+      const { error: insertError } = await supabase.from("Event").insert({
+        title,
+        description,
+        location,
+        date,
+        image_url: `https://ghzfhsfaejmxwcbmjaec.supabase.co/storage/v1/object/public/event-photo/public/${randomId}.${extension}`,
+        added_by: user.id,
+      });
+
+      if (insertError) {
+        throw new Error("Failed to create event. Please try again.");
+      }
+
+      // Redirect or navigate to the desired page
+      redirect("/");
+    } catch (error) {
+      console.error(error);
+      return redirect("/new-event?message=Something went wrong");
     }
-
-    const { error: err } = await supabase.from("Event").insert({
-      title,
-      description,
-      location,
-      date,
-      image_url: `https://ghzfhsfaejmxwcbmjaec.supabase.co/storage/v1/object/public/event-photo/public/${image.name}`,
-      added_by: user.id,
-    });
-
-    if (err) {
-      console.log(err);
-      return redirect("/new-event?message=Could not create an event");
-    }
-
-    return redirect("/");
   };
 
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-xl justify-center gap-2">
       <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground bg-[#F2F1EB] p-2 shadow-lg">
         <BackButton />
+        {searchParams?.message && (
+          <p className="mt-4 p-4 text-center bg-red-700 text-white">
+            {searchParams.message}
+          </p>
+        )}
         <label className="text-md" htmlFor="title">
           Title
         </label>
@@ -64,12 +90,13 @@ export default function Login({
           required
         />
         <label className="text-md" htmlFor="image">
-          Image
+          Image (.jpg file)
         </label>
         <input
           className="rounded-md px-4 py-2 bg-inherit border mb-6"
           type="file"
           name="image"
+          accept="image/jpeg"
           required
         />
         <label className="text-md" htmlFor="description">
@@ -98,18 +125,16 @@ export default function Login({
           className="rounded-md px-4 py-2 bg-inherit border mb-6"
           type="datetime-local"
           name="date"
+          min={new Date().toISOString().slice(0, -8)}
           required
         />
         <SubmitButton
           formAction={addEvent}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2 text-white"
           pendingText="Sending..."
         >
           Submit
         </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 text-center">{searchParams.message}</p>
-        )}
       </form>
     </div>
   );
